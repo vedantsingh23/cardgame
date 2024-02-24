9c7971cbc486d7f8806ab9ef4c62f12afa81e7ab
vedantsingh23 <141285386+vedantsingh23@users.noreply.github.com>
2024-02-23 17:34:22 -0700

    d

--- cardd.js
+++ cardd.js
@@ -1,3 +1,43 @@
+var counter = 0;
+var cards = [];
+var flipped = [];
+var matched = [];
+var starttime;
+var symbols = ['😍','🥰','😘','😗','😙','😚','😋','😛','😝','😜','🤪','🤨','🧐','🤓','😎'];
+
+function createCards(){
+    var pairs = symbols.concat(symbols); //Duplicates the array
+    pairs = suffleArray(pairs);
+    var containter = document.getElementById('card-containter');
+
+    pairs.forEach(function(symbol){
+        var card = document.createElement('div');
+        card.className = 'card'; //creates a new class
+        card.innerHTML = `<div class = 'cardInner'>
+            <div class = 'card-face card-front'></div>
+            <div class = 'card-face card-back'>${symbol}</div>`
+        card.addEventListener('click', function(){
+            if(!card.classList.contains('flipped') && flippedCards.length < 2){
+                card.classList.add('flipped')
+
+                if(flippedCards.length == 2){
+                    checkMatch
+                }
+            }
+        })
+        containter.appendChild(card)
+        cards.push(card)
+    })
+}
+
+function checkMatchingCards(){
+    counter ++;
+}
+
+function shuffleArray(){
+    
+}
+
 window.onload = function() {
     var card = document.getElementById('card');
     card.addEventListener('click', function() {
